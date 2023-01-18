93be742c24df5e37366359c0d70bd2faec7f5f9a
muhafly <guy.rslab@gmail.com>
2023-01-18 13:47:48 -0800

    7th commit

--- api/client.py
+++ api/client.py
@@ -1,6 +1,8 @@
 import requests
-
-endpoint = "http://localhost:8000/api/"
-
-get_response = requests.get(endpoint, json={"level": 2, "habit": 4})
+import time
+endpoint = "https://gameonomix.herokuapp.com/"
+tic = time.perf_counter()
+get_response = requests.get(endpoint, json={"level": 3, "habit": 2})
+toc = time.perf_counter()
+print(f"Downloaded the tutorial in {toc - tic:0.4f} seconds")
 print(get_response.json())
