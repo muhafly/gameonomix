2f166d0d15689c3d9e83fe00a5dcbb4928bddf27
muhafly <guy.rslab@gmail.com>
2023-01-25 15:56:22 -0800

    updated models - json field

--- economies/models.py
+++ economies/models.py
@@ -14,9 +14,9 @@ class Event(models.Model):
         return self.title
 
 class EconomicModel(models.Model):
-    event = models.ForeignKey(Event, on_delete=models.PROTECT, null=True, blank=True)
-    economic_model = models.TextField(blank=True, null=True)
-    economic_model_raw_data = models.TextField(blank=True, null=True)
+    event = models.ForeignKey(Event, on_delete=models.PROTECT)
+    economic_model = models.JSONField(blank=True, null=True)
+    economic_model_raw_data = models.JSONField(blank=True, null=True)
     notes = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
