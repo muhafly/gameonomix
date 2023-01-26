9a761bcd7a3b49a5dd90bf7e178a0b1c2397c879
muhafly <guy.rslab@gmail.com>
2023-01-25 17:47:14 -0800

    moved model to db

--- api/client.py
+++ api/client.py
@@ -1,8 +1,9 @@
 import requests
 import time
 endpoint = "http://gameonomix.com/"
+endpoint = "http://localhost:8000/"
 tic = time.perf_counter()
-get_response = requests.get(endpoint, json={"level": 9, "habit": 3.2})
+get_response = requests.get(endpoint, json={"level": 9, "habit": 3.2, "event": 1})
 toc = time.perf_counter()
 print(f"API responded in {toc - tic:0.4f} seconds")
 print(get_response.json())
