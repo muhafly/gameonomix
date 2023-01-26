07273d1c1160fe3d61a056b1aa8185d1af775ddc
muhafly <guy.rslab@gmail.com>
2023-01-25 16:48:35 -0800

    moved to postgres

--- gameonomix/settings.py
+++ gameonomix/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'gameonomix.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'jikSG2Htdw9IpG8uUvh1',
+        'HOST': 'containers-us-west-118.railway.app',
+        'PORT': '5479',
     }
 }
 
